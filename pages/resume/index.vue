15496644a310c1f829564489301b22f4e462561a
Emmanuel Francis Jr Ramos <ponnex@trrtech.ph>
2020-06-30 13:17:10 +0800

    Fixes for Resume page and pdf scaling

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="resume">
-    <pdf src="/ramos_resume.pdf">
+    <pdf :scale.sync="scale" src="/ramos_resume.pdf">
       <template slot="loading">
         <span class="loading">Loading pdf...</span>
       </template>
@@ -17,9 +17,12 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "nuxt-property-decorator";
+import { Vue, Component, Watch } from "nuxt-property-decorator";
 // @ts-ignore
 import pdf from "pdfvuer";
+import VueScreen from 'vue-screen';
+
+Vue.use(VueScreen);
 
 @Component({
   components: {
@@ -27,6 +30,42 @@ import pdf from "pdfvuer";
   }
 })
 export default class ResumePage extends Vue {
+  /**
+   * Scale value for PDF Component
+   */
+  scale: number | string = 'page-width';
 
+  /**
+   * Calculated pdfWidth A4 or based on current PDF used
+   */
+  pdfWidth: number = 794.6666666666666;
+
+  /**
+   * Watcher for screen width changes
+   */
+  @Watch('$screen.width')
+  onScreenWidthChange(width: number) {
+    this.resizePdf();
+  }
+
+  /**
+   * Vue mounted lifecycle hook
+   */
+  mounted() {
+    this.resizePdf();
+  }
+
+  /**
+   * Resize PDF component
+   */
+  resizePdf() {
+    this.$nextTick(() => {
+      let { browser, isTablet, isMobile } = this.$util.browserDetails();
+      let resumeEl = document.querySelector('.resume') as HTMLElement;
+      let pagePadding = parseInt(window.getComputedStyle(resumeEl, null).getPropertyValue('padding-left').match(/\d+/g)![0]) * 2;
+      let resumeWidth = (this.$screen.width - pagePadding) > 1600 ? 1600 : (this.$screen.width - pagePadding);
+      this.scale = isMobile ? (resumeWidth / this.pdfWidth) : 'page-width'; 
+    })
+  }
 }
 </script>
